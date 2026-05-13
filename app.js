e9d3f18279bfa0546af7b3726fadf4e4fd37d9b7
eyleatherorders-creator <eyleatherorders@gmail.com>
2026-05-13 12:12:36 +0300

    Add files via upload

---
+++ app.js
@@ -0,0 +1,52 @@
+import { loadUnifiedInventory ,initStores} from "./js/firebase.js";
+
+import { renderBookTypeSelector } from "./config/filters.js";
+
+import { setInventoryRows } from "./config/state.js";
+
+import { buildCatalogNumber } from "./config/catalog.js";
+
+import { setShowPrices, showPrices } from "./config/state.js";
+import { renderFilteredProducts } from "./config/products.js";
+import { updateCart } from "./config/cart.js";
+import { loadRemoteRules, CATALOG_RULES, PRICE_MATRIX } from './config/rules.js';
+
+const toggle = document.getElementById("priceToggle");
+
+toggle.addEventListener("change", () => {
+  setShowPrices(toggle.checked);
+
+  renderFilteredProducts();
+  updateCart();
+});
+
+window.addEventListener("DOMContentLoaded", async () => {
+  const success = await loadRemoteRules();
+  if(success){
+
+    const rows = await loadUnifiedInventory();
+    const processed = rows.map(row => ({
+      ...row,
+      catalogNumber: buildCatalogNumber(row)
+    }));
+
+    setInventoryRows(processed);
+
+    renderBookTypeSelector();
+    initStores();
+
+  }
+  
+});
+
+import { 
+    addToCart,
+    toggleCart,
+    submitOrder,
+    changeQty
+ } from "./config/cart.js"; // or wherever it is
+
+window.addToCart = addToCart;
+window.toggleCart = toggleCart
+window.submitOrder = submitOrder
+window.changeQty = changeQty
